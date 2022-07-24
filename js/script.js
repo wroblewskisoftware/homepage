bbc781a1b2b38d049521906a7a311af24fc36904
Radoslaw Wroblewski <wroblewski082@gmail.com>
2022-07-24 15:16:08 +0200

    changed operation of the image button

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ console.log("Cześć! Ten kod jest już w repozytorium Git.");
 
 let imageButton = document.querySelector(".js-header__imageButton");
 let imageParagraph = document.querySelector(".js-header__imageParagraph");
+let imageStatus = document.querySelector(".js-header__imageStatus");
 let backgroundButton = document.querySelector(".js-header__backgroundButton");
 let body = document.querySelector(".js-body");
 let themeName = document.querySelector(".js-header__themeName");
 
 imageButton.addEventListener("click", () => {
-    imageParagraph.remove();
+    imageButton.classList.toggle("header__imageButton--hidden");
+    imageParagraph.classList.toggle("header__imageParagraph--hidden");
+    imageStatus.innerText = imageParagraph.classList.contains("header__imageParagraph--hidden") ? "Pokaż" : "Ukryj";
 });
 
 backgroundButton.addEventListener("click", () => {
