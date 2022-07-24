d37120441e570b5ca95b7503571eac1308d75fc3
Radoslaw Wroblewski <wroblewski082@gmail.com>
2022-07-24 14:59:42 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("Cześć!");
+
+let imageButton = document.querySelector(".js-header__imageButton");
+let imageParagraph = document.querySelector(".js-header__imageParagraph");
+let backgroundButton = document.querySelector(".js-header__backgroundButton");
+let body = document.querySelector(".js-body");
+let themeName = document.querySelector(".js-header__themeName");
+
+imageButton.addEventListener("click", () => {
+    imageParagraph.remove();
+});
+
+backgroundButton.addEventListener("click", () => {
+    backgroundButton.classList.toggle("header__backgroundButton--dark");
+    body.classList.toggle("body--dark");
+    themeName.innerText = body.classList.contains("body--dark") ? "jasne" : "ciemne";
+});
